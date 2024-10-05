c9fdafae9619a29b3558c34ee26adaa9e7e4ce07
Reinier Ramos <reinierramos@Reiniers-MacBook-Air.local>
2024-10-05 16:11:51 +0800

    Updated script inheritance for HodgkinHuxley.

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ figChanA1 = HH.plotChannels(soln, t)
 figChanA1[1].set_title('Single HH with constant input')
 
 
-soln, t   = HH.solveHH(system='single', solver='lsoda', I0=2.5)
+soln, t   = HH.solveHH(system='single', solver='rl4', Is=10, fs=4.905)
 figVoltA2 = HH.plotVoltage(soln, t)
 figVoltA2[1].set_title('Single HH with sinusoid input')
 figChanA2 = HH.plotChannels(soln, t)
-figChanA2[1].set_title('Single HH with constant input')
+figChanA2[1].set_title('Single HH with sinusoid input')
 
 
 soln, t  = HH.solveHH(system='noisy', solver='euler', In=60)
 figVoltB = HH.plotVoltage(soln, t)
 figVoltB[1].set_title('Noisy HH with constant input')
 figVoltB = HH.plotChannels(soln, t)
-figVoltB[1].set_title('Single HH with constant input')
+figVoltB[1].set_title('Noisy HH with constant input')
 
 
 soln, t  = HH.solveHH(system='coupled', solver='euler', I0=10, L=3, g=0.1)
